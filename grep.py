ce7666a5d9bbad864ca4de31f9f16e2cc215a2cb
BlueSilverCat <bluesilvercat@gmail.com>
2021-04-28 16:22:52 +0900

    ver2

--- grep.py
+++ grep.py
@@ -77,6 +77,8 @@ class EntryInfo():
 ########################################################################################################################
 ## EntryList
 ########################################################################################################################
+
+
 class EntryList:
 
   def __init__(self, path, reInclude, reExclude):
@@ -141,11 +143,177 @@ class EntryList:
 
 
 ########################################################################################################################
+## MatchLineInfo
+########################################################################################################################
+
+
+class MatchLineInfo:
+
+  def __init__(self, line, start, end, length, string, invert):
+    self.line = line
+    self.start = start
+    self.end = end
+    self.length = length
+    self.string = string
+    self.invert = invert
+    # self.setPrintOption()
+
+  def __repr__(self):
+    return f"{self.line}: [{self.start}--{self.end}]({self.length})\"{self.string}\" {self.invert}"
+
+  # def setPrintOption(self, noLineNumber=False, lineNumberLength=3, noOffset=False, offsetLength=3, showZeroLength=False, showInvert=False):
+  #   self.noLineNumber = noLineNumber
+  #   self.lineNumberLength = lineNumberLength
+  #   self.noOffset = noOffset
+  #   self.offsetLength = offsetLength
+  #   self.showZeroLength = showZeroLength
+  #   self.showInvert = showInvert
+
+
+########################################################################################################################
+## MatchInfo
+########################################################################################################################
+
+
+class MatchInfo:
+
+  def __init__(self):
+    self.start = -1
+    self.end = -1
+    self.length = -1
+    self.string = None
+    self.invert = False
+    # self.lineInfo = []
+
+  def __repr__(self):
+    return f"[{self.start}--{self.end}]({self.length})\"{self.string}\" {self.invert}"
+
+  def __lt__(self, other):
+    return self.start < other.start
+
+  def setBasicInfo(self, match, lastIndex=0):
+    self.start = match.start() + lastIndex
+    self.end = match.end() + lastIndex
+    self.string = match[0]
+    self.length = len(match[0])
+    self.invert = False
+    return self
+
+  def setInfo(self, start, end, string, invert=False):
+    self.start = start
+    self.end = end
+    self.string = string
+    self.length = len(self.string)
+    self.invert = invert
+    return self
+
+
+########################################################################################################################
 ## grep
 ########################################################################################################################
 
 
-class Grep():
+class Regex:
+
+  def __init__(self, pattern, flags=0):
+    self.pattern = pattern
+    self.flags = flags
+    self.regex = None
+    self.includeZero = True
+    # self.string = string
+    self.matchInfo = []
+    self.matchLineInfo = []
+    self.compile()
+
+  def __repr__(self):
+    return f"{self.pattern}, {self.flags}, {len(self.matchInfo)}"
+
+  def compile(self):
+    self.regex = re.compile(self.pattern, self.flags)
+
+  def search(self, text, includeZero=True):
+    self.matchInfo = []
+    self.regexInfos = []
+    self.includeZero = includeZero
+    match = self.regex.search(text)
+    if match is None:
+      return
+
+    lastIndex = self.storeMatchInfo(match)
+    while len(text) >= lastIndex:
+      match = self.regex.search(text[lastIndex:])
+      if match is None:
+        return
+      lastIndex = self.storeMatchInfo(match, lastIndex)
+    return
+
+  def storeMatchInfo(self, match, lastIndex=0):
+    if self.includeZero or len(match[0]) > 0:
+      self.matchInfo.append(MatchInfo().setBasicInfo(match, lastIndex))
+    return Regex.getLastIndex(match, lastIndex)
+
+  @staticmethod
+  def getLastIndex(match, lastIndex=0):
+    end = match.end() + lastIndex
+    length = len(match[0])
+    return end if length > 0 else end + 1
+
+  def setInvert(self, string):
+    start = 0
+    work = []
+    for i in self.matchInfo:
+      if i.start > start:
+        work.append(MatchInfo().setInfo(start, i.start, string[start:i.start], True))
+      start = i.end
+    if start < len(string):
+      work.append(MatchInfo().setInfo(start, len(string), string[start:], True))
+    # self.matchInfo += work
+    self.matchInfo = work
+    self.matchInfo.sort()
+
+  @staticmethod
+  def getLineInfo(string, matchInfo, linebreak="\n"):
+    lineInfo = []
+    work = matchInfo.string
+    matchLines = split(work, linebreak)
+    lineOffset = string.count(linebreak, 0, matchInfo.start)
+    columnOffset = matchInfo.start - string[:matchInfo.start].rfind(linebreak) - 1
+    for i in range(len(matchLines)):
+      lineNumber = lineOffset + i
+      lineInfo.append(
+          MatchLineInfo(line=lineOffset + i,
+                        start=columnOffset,
+                        end=columnOffset + len(matchLines[i]),
+                        length=len(matchLines[i]),
+                        string=matchLines[i],
+                        invert=matchInfo.invert))
+      columnOffset = 0
+    return lineInfo
+
+  def setLineInfo(self, string, linebreak="\n"):
+    self.matchLineInfo = []
+    for i in self.matchInfo:
+      for j in Regex.getLineInfo(string, i, linebreak):
+        self.matchLineInfo.append(j)
+
+  def printInfo(self, invertMatch=False):
+    for i in self.matchInfo:
+      if i.invert == invertMatch:
+        print(i)
+
+  def printLineInfo(self, invertMatch=False):
+    for i in self.matchLineInfo:
+      if i.invert == invertMatch:
+        print(i)
+
+  def perform(self, data, linebreak="\n", includeZero=True, invertMatch=False):
+    self.search(data, includeZero)
+    if invertMatch:
+      self.setInvert(data)
+    self.setLineInfo(data, linebreak)
+
+
+class Grep:
 
   def __init__(self, args):
     self.setSearchOption(args)
@@ -153,34 +321,15 @@ class Grep():
     self.setFlag(args)
     self.compile()
     self.entryList = EntryList(self.path, self.reInclude, self.reExclude)
+    self.regex = Regex(self.pattern, self.flags)
 
   def setSearchOption(self, args):
     self.path = args.path
-    self.search = args.search
+    self.pattern = args.pattern
     self.repl = args.replace
     self.invertMatch = args.invertMatch
     self.include = args.include
     self.exclude = args.exclude
-
-  def setFlag(self, args):
-    self.flags = 0
-    if args.ascii:
-      self.flags += re.ASCII
-    if args.ignoreCase:
-      self.flags += re.IGNORECASE
-    # if args.multiLine:
-    #   self.flags += re.MULTILINE
-    # if args.dotAll:
-    #   self.flags += re.DOTALL
-
-  def compile(self):
-    self.reInclude = None
-    if self.include is not None:
-      self.reInclude = re.compile(self.include, flags=self.flags)
-    self.reExclude = None
-    if self.exclude is not None:
-      self.reExclude = re.compile(self.exclude, flags=self.flags)
-    self.reSearch = re.compile(self.search, flags=self.flags)
 
   def setPrintOption(self, args):
     self.tree = args.tree
@@ -192,53 +341,74 @@ class Grep():
     self.noOffset = args.noOffset
     self.offsetLength = args.offsetLength
     self.showZeroLength = args.showZeroLength
+    self.showOnlyMatch = args.showOnlyMatch
+    self.replaceTest = args.replaceTest
 
-  def main(self):
+  def setFlag(self, args):
+    self.flags = 0
+    if args.ascii:
+      self.flags += re.ASCII
+    if args.ignoreCase:
+      self.flags += re.IGNORECASE
+    if args.multiLine:
+      self.flags += re.MULTILINE
+    if args.dotAll:
+      self.flags += re.DOTALL
+
+  def compile(self):
+    self.reInclude = None
+    if self.include is not None:
+      self.reInclude = re.compile(self.include, flags=self.flags)
+    self.reExclude = None
+    if self.exclude is not None:
+      self.reExclude = re.compile(self.exclude, flags=self.flags)
+
+  def __repr__(self):
+    return f"{self.pattern}, {self.flags}, {len(self.matchInfo)}"
+
+  def perform(self):
     self.printTree()
-    for i in self.entryList.entries:
-      if not i.isFile():
+    for entryInfo in self.entryList.entries:
+      if not entryInfo.isFile():
         continue
-      data = grep.scan(i)
-      Grep.write(i, data)
+      self.printCaption(entryInfo.name)
+      with open(entryInfo.path, "r", encoding="utf-8", newline="") as file:
+        data = file.read()
+      linebreak = getLineBreak(data)
+      self.regex.perform(data, linebreak, self.showZeroLength, self.invertMatch)
 
-  def scan(self, entryInfo):
-    self.printCaption(entryInfo.name)
-    with open(entryInfo.path, "r", encoding="utf-8", newline="") as file:
-      data = file.readlines()
+      # self.regex.printLineInfo(self.invertMatch)
 
-    replaceFlag = False
-    for line, text in enumerate(data):
-      if line != len(data) - 1:  # 改行を削除しないと処理がVScodeなどで置換した場合と異なってしまう。最終行は改行を消してしまうと処理がおかしくなる
-        text, linebreak = Grep.removeLineBreak(text)
+      if self.showOnlyMatch:
+        self.printMessageOnly(entryInfo.name)
       else:
-        linebreak = ""
-      matchList = search(self.reSearch, text, self.showZeroLength)
-      if ((len(matchList) > 0 and not self.invertMatch) or (len(matchList) <= 0 and self.invertMatch)):
-        self.printMessage(entryInfo.name, line, text, matchList)
-      replaceFlag = True if self.replace(data, line, text, linebreak) else replaceFlag
-    return data if replaceFlag else []
+        self.printMessage(entryInfo.name, data, linebreak)
+      replaceFlag, data = self.replace(data)
+      if replaceFlag:
+        if self.replaceTest:
+          print(data)
+        else:
+          self.write(entryInfo.path, data)
 
-  def replace(self, data, line, text, linebreak):
-    match = self.reSearch.search(text)
-    if match is not None:
-      if int(match.group(1)) > int(match.group(2)):
-        result = self.reSearch.sub('min="\\2" max="\\1"', text) + linebreak
-        print(f"####{result}")
-        data[line] = result
-        return True
-    return False
+  def replace(self, string):
+    if self.repl is None:
+      return (False, string)
 
-  # def replace(self, data, line, text, linebreak):
-  #   if self.repl is None:
-  #     return False
+    if not self.invertMatch and len(self.regex.matchInfo) > 0:
+      result = self.regex.regex.sub(self.repl, string)
+      return (True, result)
+    elif self.invertMatch:
+      result = self.replaceRange(string)
+      return (True, result)
+    return (False, string)
 
-  #   if not self.invertMatch and self.reSearch.search(text) is not None:
-  #     data[line] = self.reSearch.sub(self.repl, text) + linebreak
-  #     return True
-  #   elif self.invertMatch and self.reSearch.search(text) is None:
-  #     data[line] = self.repl
-  #     return True
-  #   return False
+  def replaceRange(self, string):
+    work = string
+    length = len(string)
+    for info in self.regex.matchInfo:
+      diff = len(work) - length
+      work = work[:info.start + diff] + self.repl + work[info.end + diff:]
+    return work
 
   ## print
 
@@ -251,34 +421,53 @@ class Grep():
     if not self.noCaption:
       print(f"# {name} #")
 
-  def printMessage(self, name, line, text, matchList):
+  def getFileName(self, name):
+    return f"{name:{self.fileNameLength}}" if not self.noFileName else ""
+
+  def getLineNumber(self, info):
+    return f"[{info.line:0{self.lineNumberLength}}]" if not self.noLineNumber else ""
+
+  def getOffset(self, info):
+    return f"({info.start:0{self.offsetLength}}:{info.length:0{self.offsetLength}})" if not self.noOffset else ""
+
+  def getSeparetor(self):
+    return f":" if not self.noFileName or not self.noLineNumber or not self.noOffset else ""
+
+  def printMessage(self, name, text, linebreak="\n", replacer=""):  # replacer="↲"
+    lineText = split(text, linebreak, replacer)
+    previousLine = 0
     string = ""
-    if not self.noFileName:
-      string = f"{name:{self.fileNameLength}}"
-    if not self.noLineNumber:
-      string += f"[{line:0{self.lineNumberLength}}]"
-    if not self.noOffset:
-      for match in matchList:
-        # string += f"({match['start']:0{self.offsetLength}}--{match['end']:0{self.offsetLength}}:{match['length']})"
-        string += f"({match['start']:0{self.offsetLength}}:{match['length']:0{self.offsetLength}})"
-    if not self.noFileName or not self.noLineNumber or not self.noOffset:
-      string += f":"
-    text, _ = Grep.removeLineBreak(text)
-    string += f"{text}"
+    offsetString = ""
+    for info in self.regex.matchLineInfo:
+      # print(info)
+      if info.line != previousLine:
+        print(string)
+        offsetString = ""
+        string = ""
+      string = self.getFileName(name)
+      string += self.getLineNumber(info)
+      offsetString = offsetString + self.getOffset(info)
+      if not self.noOffset:
+        string += f"{offsetString}"
+      string += self.getSeparetor()
+      string += f"{lineText[info.line]}"
+      previousLine = info.line
     print(string)
 
-  @staticmethod
-  def write(entryInfo, data):
-    if len(data) == 0:
-      return
-    with open(entryInfo.path, "w", encoding="utf-8", newline="") as file:  # 改行コード
-      file.writelines(data)
+  def printMessageOnly(self, name, linebreak="\n", replacer=""):  # replacer="↲"
+    string = ""
+    for info in self.regex.matchLineInfo:
+      string = self.getFileName(name)
+      string += self.getLineNumber(info)
+      string += self.getOffset(info)
+      string += self.getSeparetor()
+      string += f"{info.string.replace(linebreak, replacer)}"
+      print(string)
 
   @staticmethod
-  def removeLineBreak(string):
-    linebreak = checkTailLineBreak(string)
-    string = removeLineBreak(string, linebreak)
-    return (string, linebreak)
+  def write(path, string):
+    with open(path, "w", encoding="utf-8", newline="") as file:  # 改行コード
+      file.write(string)
 
 
 ########################################################################################################################
@@ -306,43 +495,21 @@ def checkTailLineBreak(string):
   return ""
 
 
-########################################################################################################################
-## regex
-########################################################################################################################
+def getLineBreak(string):
+  crlfCount = string.count("\r\n")
+  lfCount = string.count("\n")
+  return "\r\n" if crlfCount != 0 and crlfCount == lfCount else "\n"
 
 
-def search(regex, text, includeZero=True):
-  match = regex.search(text)
-  if match is None:
-    return []
-
-  lt = []
-  lastIndex = storeMatchInfo(lt, match, 0, includeZero)
-  while len(text) >= lastIndex:
-    match = regex.search(text[lastIndex:])
-    if match is None:
-      return lt
-    lastIndex = storeMatchInfo(lt, match, lastIndex, includeZero)
-  return lt
-
-
-def storeMatchInfo(lt, match, lastIndex=0, includeZero=True):
-  if includeZero or len(match[0]) > 0:
-    lt.append(getMatchInfo(match, lastIndex))
-  return getLastIndex(match, lastIndex)
-
-
-def getMatchInfo(match, lastIndex=0):
-  start = match.start() + lastIndex
-  end = match.end() + lastIndex
-  length = len(match[0])
-  return {"start": start, "end": end, "length": length, "string": match[0]}
-
-
-def getLastIndex(match, lastIndex=0):
-  end = match.end() + lastIndex
-  length = len(match[0])
-  return end if length > 0 else end + 1
+def split(string, separator="\n", replacer="\n"):  # replacer="↲"
+  if string == "":
+    return [string]
+  work = string.split(separator)
+  for i in range(len(work) - 1):
+    work[i] += replacer
+  # if work[len(work) - 1] == "":
+  #   work.pop()
+  return work
 
 
 ########################################################################################################################
@@ -353,8 +520,8 @@ def getLastIndex(match, lastIndex=0):
 # re.ASCII
 # re.DEBUG: 無効
 # re.IGNORECASE
-# re.MULTILINE: 無効
-# re.DOTALL: 無効
+# re.MULTILINE
+# re.DOTALL:
 # re.VERBOSE: 無効
 
 
@@ -362,21 +529,32 @@ def argumentParser():
   parser = argparse.ArgumentParser()
   # search option
   parser.add_argument("path", help="specify file or directory.")
-  parser.add_argument("search", help="specify search string as regular expression.")
-  parser.add_argument("-r", "--replace", nargs="?", const="", help="specify replace string.")  # 空文字列を受け取れる設定
+  parser.add_argument("pattern", help="specify search string as regular expression.")
+  parser.add_argument("-r", "--replace", nargs="?", const="", help="specify replace string. replace matching strings with this string.")  # 空文字列を受け取れる設定
+  parser.add_argument("-rt", "--replaceTest", action="store_true",  help="Show only the result of replacement.")
   parser.add_argument("-v", "--invertMatch", action="store_true", help="select non-matching lines.")
   parser.add_argument("-i", "--include", help="search only path that match pattern.")
   parser.add_argument("-e", "--exclude", help="skip path match pattern.")
 
   # match option
   parser.add_argument(
-      "-A", "--ascii", action="store_true",
+      "-A",
+      "--ascii",
+      action="store_true",
       help="make \\w, \\W, \\b, \\B, \\d, \\D, \\s and \\S perform ASCII-only matching instead of full Unicode matching."
   )
   parser.add_argument("-I", "--ignoreCase", action="store_true", help="perform case-insensitive matching.")
-  # parser.add_argument("-m", "--multiLine", action="store_true", help="when specified, the pattern character '^' matches at the beginning of the string and at the beginning of each line (immediately following each newline).")
-  # parser.add_argument("-D", "--dotAll", action="store_true",
-  #                     help="make the '.' special character match any character at all, including a newline.")
+  parser.add_argument(
+      "-M",
+      "--multiLine",
+      action="store_true",
+      help=
+      "when specified, the pattern character '^' matches at the beginning of the string and at the beginning of each line (immediately following each newline)."
+  )
+  parser.add_argument("-D",
+                      "--dotAll",
+                      action="store_true",
+                      help="make the '.' special character match any character at all, including a newline.")
 
   # tree view
   parser.add_argument("-t", "--tree", action="store_true", help="show directory tree.")
@@ -390,7 +568,8 @@ def argumentParser():
   parser.add_argument("-fl", "--fileNameLength", type=int, default=1, help="file name length.")
   parser.add_argument("-o", "--noOffset", action="store_true", help="no print offset with output lines.")
   parser.add_argument("-ol", "--offsetLength", type=int, default=3, help="offset length.")
-  parser.add_argument("-z", "--showZeroLength", action="store_true", help="show zero length match.")  ## fix
+  parser.add_argument("-z", "--showZeroLength", action="store_true", help="show zero length match.")
+  parser.add_argument("-O", "--showOnlyMatch", action="store_true", help="show only matched strings.")
   return parser.parse_args()
 
 
@@ -404,4 +583,4 @@ if __name__ == "__main__":
     print(args)
 
   grep = Grep(args)
-  grep.main()
+  grep.perform()
